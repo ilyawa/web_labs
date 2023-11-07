3b36c22db4c5f37153a4886237c8da53a156dd1c
ilyadr2 <71603061+ilyadr2@users.noreply.github.com>
2023-11-07 20:14:35 +0300

    lab 4 done

--- lab4/login_script.php
+++ lab4/login_script.php
@@ -1,1 +1,50 @@
 <?php
+$db = new SQLite3('users.db');
+
+// Создание таблицы users, если она не существует
+$db->exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, inverted_password TEXT)');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Проверка наличия пользователя в базе данных
+$stmt = $db->prepare('SELECT COUNT(*) as count FROM users WHERE username = :username');
+$stmt->bindValue(':username', $username, SQLITE3_TEXT);
+$result = $stmt->execute();
+$row = $result->fetchArray();
+
+
+function invertBitsInString($str) {
+    $result = '';
+    $len = strlen($str);
+    for ($i = 0; $i < $len; $i++) {
+        // Convert character to ASCII value
+        $ascii = ord($str[$i]);
+        // Convert ASCII value to binary string
+        $bin = decbin($ascii);
+        // Pad binary string with zeros on the left to ensure it's 8 bits long
+        $bin = str_pad($bin, 8, "0", STR_PAD_LEFT);
+        // Invert bits in binary string
+        $bin = $bin ^ str_repeat('1', 8);
+        // Convert inverted binary string back to string
+        $result .= chr(bindec($bin));
+    }
+    return $result;
+}
+
+
+if ($row['count'] == 0) {
+    // Если пользователя нет, добавляем его в базу данных
+    $invertedPassword = invertBitsInString($password); // Инвертируем биты пароля
+    $stmt = $db->prepare('INSERT INTO users (username, password, inverted_password) VALUES (:username, :password, :invertedPassword)');
+    $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+    $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+    $stmt->bindValue(':invertedPassword', $invertedPassword, SQLITE3_TEXT);
+    $stmt->execute();
+}
+
+$db->close();
+
+// Перенаправляем пользователя на страницу для ввода дополнительных данных
+header("Location: order.html");
+?>
--- lab4/order_script.php
+++ lab4/order_script.php
@@ -1,1 +1,30 @@
 <?php
+$db = new SQLite3('users.db');
+
+// Создание таблицы orders, если она не существует
+$db->exec('CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, lastname TEXT, firstname TEXT, middlename TEXT, address TEXT, phone TEXT, email TEXT, product TEXT, comment TEXT)');
+
+$lastname = $_POST['lastname'];
+$firstname = $_POST['firstname'];
+$middlename = $_POST['middlename'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$product = $_POST['product'];
+$comment = $_POST['comment'];
+
+// Вставка данных о заказе в базу данных
+$stmt = $db->prepare('INSERT INTO orders (lastname, firstname, middlename, address, phone, email, product, comment) VALUES (:lastname, :firstname, :middlename, :address, :phone, :email, :product, :comment)');
+$stmt->bindValue(':lastname', $lastname, SQLITE3_TEXT);
+$stmt->bindValue(':firstname', $firstname, SQLITE3_TEXT);
+$stmt->bindValue(':middlename', $middlename, SQLITE3_TEXT);
+$stmt->bindValue(':address', $address, SQLITE3_TEXT);
+$stmt->bindValue(':phone', $phone, SQLITE3_TEXT);
+$stmt->bindValue(':email', $email, SQLITE3_TEXT);
+$stmt->bindValue(':product', $product, SQLITE3_TEXT);
+$stmt->bindValue(':comment', $comment, SQLITE3_TEXT);
+$stmt->execute();
+
+$db->close();
+header("Location: order.html");
+?>
